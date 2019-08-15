846f937defe1f07455c78cb92a58737605ff22b3
yamachu <yamachu.dev@gmail.com>
2019-08-15 23:07:29 +0900

    move lib dir

--- frontend/src/lib/hooks/firebase.js
+++ frontend/src/lib/hooks/firebase.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import firebase from "../../firebase";
+import firebase from "../firebase";
 
 export default () => {
   const [user, setUser] = useState(null);
