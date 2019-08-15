457f39846f8ce0d2641f4e99caef7c389d9b082f
yamachu <yamachu.dev@gmail.com>
2019-08-15 23:25:41 +0900

    こんな感じ

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useMemo } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Header from "./Layout/Header";
 import { useFirebase } from "./lib/hooks";
 import UserContext from "./lib/UserContext";
+import { MessagePrivate, MessagePublic } from "./Message/View";
 
 export default () => {
   const { user, login, logout } = useFirebase();
@@ -18,6 +20,12 @@ export default () => {
   return (
     <UserContext.Provider value={{ user: userObject }}>
       <Header {...{ login, logout }} />
+      <HashRouter>
+        <Switch>
+          <Route path={"/public"} component={MessagePublic} />
+          <Route path={"/private"} component={MessagePrivate} />
+        </Switch>
+      </HashRouter>
     </UserContext.Provider>
   );
 };
