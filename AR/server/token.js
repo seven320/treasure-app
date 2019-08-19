b4e8350c4d2e1e46491120b1b2028d56ef7d66d7
seven320 <yosyuaomenw@yahoo.co.jp>
2019-08-19 16:06:53 +0900

    test

---
+++ AR/server/token.js
@@ -0,0 +1,19 @@
+// const twittertoken = "hoge"
+
+
+
+// const client = new Twitter({
+//     cosumer_key:"cXeXwTgOlx8jsSIpygQDpT6JI",
+//     consumer_secret:"j8hF0NnMfPhcU8JjSHnCsQUnTzkkIFZvRKtsYYTaPirKTTUw9h",
+//     access_toke_key:"1145375891356151809-SYBtZOPgIGby3FXOw2p9eqr6Fw8Tid",
+//     access_token_secret:"kJ1d7qsjVVnblKERQYS4k9N68eoYZGmafl5jHVZg0HHkn"
+// })
+
+exports.twitter = client
+
+const Twitter = require('twitter')
+
+var credentials = require("./secret.json");
+
+
+
---
+++ AR/server/token.mjs
@@ -0,0 +1,11 @@
+const twittertoken = "hoge"
+
+export{twittertoken}
+
+
+// {
+//     "cosumer_key":"cXeXwTgOlx8jsSIpygQDpT6JI",
+//     "consumer_secret":"j8hF0NnMfPhcU8JjSHnCsQUnTzkkIFZvRKtsYYTaPirKTTUw9h",
+//     "access_toke_key":"1145375891356151809-SYBtZOPgIGby3FXOw2p9eqr6Fw8Tid",
+//     "access_token_secret":"kJ1d7qsjVVnblKERQYS4k9N68eoYZGmafl5jHVZg0HHkn"
+// }
